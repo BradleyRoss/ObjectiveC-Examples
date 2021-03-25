34ae2f1ad6357dbc9ba2e3b5ce8b3de0a5f93340
Bradley Ross <bradleyross@me.com>
2021-03-24 21:28:46 -0400

    Changes for CoreMIDI usage

---
+++ BRossTools/BRossTools/BRossTools/CoreMIDIsamples.h
@@ -0,0 +1,12 @@
+//
+//  CoreMIDIsamples.h
+//  BRossTools
+//
+//  Created by Bradley Ross on 3/18/21.
+//
+
+#ifndef CoreMIDIsamples_h
+#define CoreMIDIsamples_h
+
+
+#endif /* CoreMIDIsamples_h */
---
+++ BRossTools/BRossTools/BRossTools/CoreMIDIsamples.m
@@ -0,0 +1,8 @@
+//
+//  CoreMIDIsamples.m
+//  BRossTools
+//
+//  Created by Bradley Ross on 3/18/21.
+//
+
+#import <Foundation/Foundation.h>
