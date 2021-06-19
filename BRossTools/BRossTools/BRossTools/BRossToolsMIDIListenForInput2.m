5c528d1eb3cf22b1491e8e17f946dc2fedf9812e
Bradley Ross <bradleyross@me.com>
2021-06-19 16:24:43 -0400

    Minor change

--- BRossTools/BRossTools/BRossTools/BRossToolsMIDIListenForInput2.m
+++ BRossTools/BRossTools/BRossTools/BRossToolsMIDIListenForInput2.m
@@ -156,6 +156,7 @@
 
 // void runWithWindow(BRossToolsTextWindow * window);
 /*
+ /Users/bradleyross/Documents/GitHub/Reload/Untitled/ObjectiveC-Examples/BRossTools/BRossTools/MDFILE.md
  Can modules called from CoreMIDI call textWindow methods -- crashing when posting to textWindow with link library problem
  */
 // *****  *****  *****  *****  *****  *****  *****  *****
@@ -353,9 +354,12 @@ if (numberOfSources == 0) {
         // UInt16 nBytes;
         // NSLog(@"First time: %llx   ", firstTime);
         const MIDIPacket *packet = &pktList->packet[0];
-        
+        /* position is location of first byte in MIDI command in packet */
+        unsigned int position;
+        /*  size is the number of bytes in the MIDI command in the packet*/
+        unsigned int size;
         for (unsigned int i = 0; i < pktList->numPackets; i++) {
-            
+            position = 0;
             Byte data[256];
             UInt16 nBytes = packet->length;
             // UInt16 processedBytes = 0;
@@ -373,44 +377,148 @@ if (numberOfSources == 0) {
             [localWindow appendString:message];
            
             // describeNote(data, nBytes, localWindow);
-            
+        
+
             if (self->extraLogMessages) {
                 NSLog(@"MIDITimeStamp %f seconds", seconds);
                 NSLog(@"There are %d data bytes in packet", nBytes);
             }
-            unsigned int firstByte = (unsigned int)data[0];
+            unsigned int firstByte = (unsigned int)data[position + 0];
             // NSLog(@"First data byte is %x", firstByte);
             unsigned int channel = firstByte % 16;
             unsigned int command = firstByte / 16;
             NSString *desc;
             if (command == 9) {
                 desc =@"NOTE ON";
-                /* second byte is pitch, third byte is velocity*/
-                message = [[NSString alloc] initWithFormat:@"      c %d %@ ch %d p %d v:%d \n", command, desc, channel, (unsigned int)data[1], (unsigned int)data[2]];
-                [self->synth startNote:(unsigned int)data[1]
-                    withVelocity:(unsigned int)data[2] onChannel:channel];
+                size = 3;
+                /* second byte is pitch, third byte is velocity */
+                message = [[NSString alloc] initWithFormat:@"      c %d %@ ch %d p %d v:%d \n", command, desc, channel, (unsigned int)data[position + 1], (unsigned int)data[position + 2]];
+                [self->synth startNote:(unsigned int)data[position + 1]
+                    withVelocity:(unsigned int)data[position + 2] onChannel:channel];
             } else if (command == 8) {
                 desc = @"NOTE OFF";
-                message = [[NSString alloc] initWithFormat:@"      c %d  %@ ch %d p %d  v %d \n", command, desc, channel, (unsigned int)data[1], (unsigned int)data[2]];
+                size = 3;
+                message = [[NSString alloc] initWithFormat:@"      c %d  %@ ch %d p %d  v %d \n", command, desc, channel, (unsigned int)data[position + 1], (unsigned int)data[position + 2]];
                 [self->synth stopNote:(unsigned int)data[1] onChannel:channel];
             } else if (command == 10) {
                 desc =@"Poly key pressure";
+                size = 3;
                 message = [[NSString alloc] initWithFormat:@" comm %d %@ ch %d key %d  pressure %d  \n", command, desc, channel, (unsigned int)data[1], (unsigned int) data[2]];
             } else if (command == 11) {
                 desc =@"CONTROL CHANGE";
+                size = 3;
                 message = [[NSString alloc] initWithFormat:@" comm %d %@ ch %d control: %d  value: %d  \n", command, desc, channel, (unsigned int)data[1], (unsigned int) data[2]];
                 
             } else if (command == 12) {
                 // only two bytes
                 desc = @"PROGRAM CHANGE";
+                /* Is size 2 or 3?  */
                 /* second byte indicates instrument */
                 message = [[NSString alloc] initWithFormat:@"      c %d  %@ ch %d inst %d  \n", command, desc, channel, (unsigned int)data[1]];
             } else if (command == 13) {
                 desc =@"CHANNEL PRESSURE";
-                message = [[NSString alloc] initWithFormat:@" comm %d %@ ch %d   value: %d  \n", command, desc, channel, (unsigned int)data[1]];
+                /*  Is size 2 or 3? */
+                message = [[NSString alloc] initWithFormat:@" comm %d %@ ch %d   value: %d  \n", command, desc, channel, (unsigned int)data[position + 1]];
             } else if (command == 14) {
                 desc =@"PITCH BEND";
-                message = [[NSString alloc] initWithFormat:@" comm %d %@ ch %d LSB:%d  MSB:%d  \n", command, desc, channel, (unsigned int)data[1], (unsigned int) data[2]];
+                size = 3;
+                message = [[NSString alloc] initWithFormat:@" comm %d %@ ch %d LSB:%d  MSB:%d  \n", command, desc, channel, (unsigned int)data[position + 1], (unsigned int) data[position + 2]];
+ 
+                
+            } else if (firstByte == 0xF0) {
+                desc = @ "MIDI System Exclusive Message (start)";
+                message = [[NSString alloc] initWithFormat:@"   %@ \n", desc];
+                
+            } else if (firstByte == 0xF1) {
+                desc = @"MIDI Quarter Frame Message";
+                message = [[NSString alloc] initWithFormat:@"   %@ \n", desc];
+                /*
+                 * There is one data byte in this command.  Is the size of the
+                 * command padded in the packet.
+                 */
+                
+            } else if (firstByte == 0xF2) {
+                desc = @"MIDI Song Position Pointer Message";
+                message = [[NSString alloc] initWithFormat:@"   %@  \n", desc];
+                size = 3;
+                /*
+                 * There are two data bytes, with each byte containing 7 bits
+                 * of a 14 bit integer.
+                 * Can there be multiple commands in a packet containing this command?
+                 */
+                
+            } else if (firstByte == 0xF3) {
+                desc = @"MIDI Song Request Message";
+                message = [[NSString alloc] initWithFormat:@"   %@ \n", desc];
+                /*
+                 * There is one data byte containing the number of the song.  Is size
+                 * in packet for this command padded.
+                 * Can there be multiple commands in a packet containing this command?
+                 */
+                
+            } else if (firstByte == 0xF6) {
+                desc = @"MIDI Tune Request Message";
+                message = [[NSString alloc] initWithFormat:@"%@", desc];
+                /*
+                 * There are no data bytes.  Is size in packet for this command padded.
+                 * Can there be multiple commands in a packet containing this command?
+                 */
+                
+            } else if (firstByte == 0xF7) {
+                desc = @"SysEX Continuation Message";
+                message = [[NSString alloc] initWithFormat:@"%@", desc];
+                /*
+                 * there is a variable number of bytes in this message.
+                 */
+                
+            } else if (firstByte == 0xF8) {
+                desc = @"MIDI Clock message";
+                message = [[NSString alloc] initWithFormat:@"%@", desc];
+                /*
+                 * There are no data bytes.  Is size in packet for this command padded.
+                 * Can there be multiple commands in a packet containing this command?
+                 */
+                
+            } else if (firstByte == 0xFA) {
+                desc = @"MIDI Start Message";
+                message = [[NSString alloc] initWithFormat:@"%@", desc];
+                /*
+                 * There are no data bytes.  Is size in packet for this command padded.
+                 * Can there be multiple commands in a packet containing this command?
+                 */
+                
+            } else if (firstByte == 0xFB) {
+                desc = @"MIDI Continue Message";
+                message = [[NSString alloc] initWithFormat:@"%@", desc];
+                /*
+                 * There are no data bytes.  Is size in packet for this command padded.
+                 * Can there be multiple commands in a packet containing this command?
+                 */
+                
+            } else if (firstByte == 0xFC) {
+                desc = @"MIDI Stop Message";
+                message = [[NSString alloc] initWithFormat:@"%@", desc];
+                /*
+                 * There are no data bytes.  Is size in packet for this command padded.
+                 * Can there be multiple commands in a packet containing this command?
+                 */
+                
+            } else if (firstByte == 0xFE) {
+                desc = @"MIDI Active Sense Message";
+                message = [[NSString alloc] initWithFormat:@"%@", desc];
+                /*
+                 * There are no data bytes.  Is size in packet for this command padded.
+                 * Can there be multiple commands in a packet containing this command?
+                 */
+                
+            } else if (firstByte == 0xFF) {
+                desc =@"MIDI Reset Message";
+                message = [[NSString alloc] initWithFormat:@"%@", desc];
+                /*
+                 * There are no data bytes.  Is size in packet for this command padded.
+                 * Can there be multiple commands in a packet containing this command?
+                 */
+                
             } else {
                 desc =  @"???";
                 message = [[NSString alloc] initWithFormat:@"      comm %d  %@ ch %d   \n", command, desc, channel];
