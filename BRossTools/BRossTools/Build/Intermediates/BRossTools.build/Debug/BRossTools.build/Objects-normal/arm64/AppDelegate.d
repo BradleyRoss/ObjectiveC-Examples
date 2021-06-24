8c39e20329236e6f8eef654ff59ca9f05ea168ba
Bradley Ross <bradleyross@me.com>
2021-06-23 20:48:21 -0400

    Removing non documentation files

--- BRossTools/BRossTools/Build/Intermediates/BRossTools.build/Debug/BRossTools.build/Objects-normal/arm64/AppDelegate.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/bradleyross/Documents/GitHub/Reload/Untitled/ObjectiveC-Examples/BRossTools/BRossTools/BRossTools/AppDelegate.m \
-  /Users/bradleyross/Documents/GitHub/Reload/Untitled/ObjectiveC-Examples/BRossTools/BRossTools/BRossTools/AppDelegate.h \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.3.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.3.sdk/usr/include/mach-o/dyld.modulemap \
-  /Users/bradleyross/Documents/GitHub/Reload/Untitled/ObjectiveC-Examples/BRossTools/BRossTools/BRossTools/BRossTools.h \
-  /Users/bradleyross/Documents/GitHub/Reload/Untitled/ObjectiveC-Examples/BRossTools/BRossTools/BRossTools/LocalClasses.h \
-  /Users/bradleyross/Documents/GitHub/Reload/Untitled/ObjectiveC-Examples/BRossTools/BRossTools/BRossTools/BRossToolsMIDI.h
